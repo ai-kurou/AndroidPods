7998a719979651985920e59a2f91e0476b8de46c
yusuke.saitou <ai-kurou@gmail.com>
2026-04-07 13:30:46 +0900

    通知のアイコン変更

--- core/service/src/main/java/kurou/androidpods/core/service/DeviceScanService.kt
+++ core/service/src/main/java/kurou/androidpods/core/service/DeviceScanService.kt
@@ -111,7 +111,7 @@ class DeviceScanService : Service() {
 
     private fun buildNotification(contentText: String, devices: List<AppleDevice>): Notification {
         val builder = Notification.Builder(this, CHANNEL_ID)
-            .setSmallIcon(android.R.drawable.stat_sys_data_bluetooth)
+            .setSmallIcon(R.drawable.ic_notification)
             .setContentTitle(getString(R.string.notification_title))
             .setContentText(contentText)
             .setOngoing(true)
